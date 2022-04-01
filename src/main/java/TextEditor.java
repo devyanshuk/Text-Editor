841594611f535b7569da856e56826ab67b5fbba3
devyanshuk <devkoirala10@gmail.com>
2022-04-01 23:27:49 +0200

    first commit, init jframe

---
+++ src/main/java/TextEditor.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class TextEditor extends JFrame {
+
+    private JPanel _panel;
+
+    public TextEditor(String title) {
+        super(title);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(_panel);
+        this.pack();
+    }
+
+    public static void main(String[] args) {
+        var frame = new TextEditor("Title");
+        frame.setVisible(true);
+    }
+}
