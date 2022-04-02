569098f7f9c643124f11c4c307bbe7962ea76f9d
devyanshuk <devkoirala10@gmail.com>
2022-04-03 01:33:33 +0200

    updates

---
+++ src/main/java/com/ICommon.java
@@ -0,0 +1,6 @@
+package com;
+
+public interface ICommon {
+    int PORT = 8000;
+    String HOST = "localhost";
+}
--- src/main/java/com/Server/Server.java
+++ src/main/java/com/Server/Server.java
@@ -1,8 +1,33 @@
 package com.Server;
 
-public class Server implements IServer {
+import com.Client.IClient;
+import com.ICommon;
+import utils.editor.TextEditor;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.net.*;
+import java.util.ArrayList;
 
-    public static void main(String[] args) {
-        System.out.println("com/Server");
+
+public class Server
+        extends Thread
+        implements IServer, ICommon {
+
+    private ServerSocket _server;
+    private ArrayList<IClient> _clients;
+
+    public Server(ServerSocket server) {
+        _server = server;
+    }
+
+    public void run() {
+        while (true) {
+
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        var server = new Server(new ServerSocket(PORT));
+        server.start();
     }
 }
